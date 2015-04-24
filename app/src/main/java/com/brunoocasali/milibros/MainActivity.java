6c667fab4ecbacb5a0c85d8cff3cdc38edfe1776
Bruno Casali <brunoocasali@gmail.com>
2015-04-24 20:54:03 -0300

    added the layout for add a new libro

--- app/src/main/java/com/brunoocasali/milibros/MainActivity.java
+++ app/src/main/java/com/brunoocasali/milibros/MainActivity.java
@@ -1,39 +1,97 @@
 package com.brunoocasali.milibros;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.AdapterView;
+import br.edu.unisep.gerenciadorzote.adapter.TarefaCursorAdapter;
+import br.edu.unisep.gerenciadorzote.model.TarefaDAO;
+import br.edu.unisep.gerenciadorzote.vo.TarefaVO;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+
+    private TarefaCursorAdapter adapter;
+    private Cursor cursor;
+    private TarefaDAO dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dao = new TarefaDAO(this);
+
+        cursor = dao.listar();
+
+        adapter = new TarefaCursorAdapter(this, cursor, 0);
+
+        setListAdapter(adapter);
+
+
+        registerForContextMenu(getListView());
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_contexto, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mn_excluir) {
+            AdapterView.AdapterContextMenuInfo menuInf = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            long id = menuInf.id;
+
+            dao.excluir(id);
+
+            cursor = dao.listar();
+            adapter.changeCursor(cursor);
+            adapter.notifyDataSetChanged();
+        }
+        return true;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        MenuInflater inflater = getMenuInflater();
+
+
+        inflater.inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == R.id.mn_novo) {
+            Intent intent = new Intent(this, InclusaoTarefaActivity.class);
+            startActivityForResult(intent, 0);
+        }
+        return true;
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        TarefaVO trabalho = (TarefaVO) data.getSerializableExtra("Tarefa");
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+
+            cursor = dao.listar();
+            adapter.changeCursor(cursor);
+            adapter.notifyDataSetChanged();
         }
 
-        return super.onOptionsItemSelected(item);
+        adapter.notifyDataSetChanged();
     }
+
 }
---
+++ app/src/main/java/com/brunoocasali/milibros/model/TarefaDAO.java
@@ -0,0 +1,51 @@
+package com.brunoocasali.milibros.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import com.brunoocasali.milibros.vo.TarefaVO;
+
+/**
+ * Created by ESutil on 21/04/2015.
+ */
+public class TarefaDAO {
+
+    private TarefaHelper helper;
+
+    public TarefaDAO(Context context){
+        this.helper = new TarefaHelper(context, "TAREFAS", null, 1);
+    }
+
+    public void salvar(TarefaVO tarefa){
+        // possibilita a aplicação gravar no banco
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues valores = new ContentValues();
+        valores.put("titulo", tarefa.getTitulo());
+        valores.put("descricao", tarefa.getDescricao());
+        valores.put("prazo", tarefa.getPrazo().getTime());
+
+        db.insert("tarefas", null, valores);
+        db.close();
+    }
+
+    public Cursor listar(){
+        // Comunicação somente leitura com o banco.
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String[] colunas = {"_id", "titulo", "descricao", "prazo"};
+
+        return db.query("tarefas", colunas, null, null, null, null, null);
+    }
+
+    public void excluir(long id){
+
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        String[] valoresWhere = {String.valueOf(id)};
+
+        db.delete("tarefas", "_id = ?", valoresWhere);
+        db.close();
+    }
+}
